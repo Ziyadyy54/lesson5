0fb160515779846bd5546826ab4c398b2d12428e
ziyad <mziyadbsk@gmail.com>
2024-09-07 19:45:04 +0300

    lesson 5

---
+++ EvenorOdd.py
@@ -0,0 +1,5 @@
+usersnumber =int(input("enter your number to know if its even or odd "))
+if usersnumber%2==0:
+    print("your number is even")
+else:
+    print("your number is odd")
---
+++ Greater.py
@@ -0,0 +1,6 @@
+firstnumber =int(input("write your first number "))
+secondnumber =int(input("write you second number "))
+if firstnumber>secondnumber:
+    print(firstnumber,"is greater than ",secondnumber)
+else:
+    print(firstnumber,"is less than ",secondnumber)
